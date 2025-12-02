daa4ace76c904336a28c09763f9790c54574b685
Axel <66166281+axelv71@users.noreply.github.com>
2025-12-02 22:32:30 +0100

    Update initLiveStats() execution to include '/en' and '/fr' paths; ensure bundle.js is updated for consistency.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import initLiveStats from './features/live-stats.js';
-import initPosthog from './features/posthog.js';
+import initLiveStats from "./features/live-stats.js";
+import initPosthog from "./features/posthog.js";
 
 initPosthog();
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Ne lance initLiveStats() que si on est à la racine du site
-    if (window.location.pathname === '/' || window.location.pathname === '/index.html' || window.location.pathname === '/landing') {
-        initLiveStats();
-    }
-})
+document.addEventListener("DOMContentLoaded", () => {
+  // Ne lance initLiveStats() que si on est à la racine du site
+  if (
+    window.location.pathname === "/" ||
+    window.location.pathname === "/index.html" ||
+    window.location.pathname === "/landing" ||
+    window.location.pathname === "/en" ||
+    window.location.pathname === "/fr"
+  ) {
+    initLiveStats();
+  }
+});
